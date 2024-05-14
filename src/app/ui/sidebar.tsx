f690e858b3c421265dd82fc3208e749ad30d9562
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-05-15 00:00:38 +0300

    Add playlist

--- src/app/ui/sidebar.tsx
+++ src/app/ui/sidebar.tsx
@@ -20,14 +20,28 @@ const navMenu = [
     icon: MdLibraryMusic
   }
 ]
+
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    href: "/",
+    icon: MdPlaylistAdd
+  }, {
+    name: "Favourite",
+    href: "/favourite",
+    icon: MdFavorite
+  }
+]
+const playlist = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
+
 const Sidebar = () => {
   return (
     <div className="w-full h-[calc(100vh-100px)] px-1.5">
-      <div className="py-5 " >
+      <div className="py-5 h-full" >
         <div className="w-48 px-4 mb-4 ">
           <NextImage src="/lofi_light1.png" width={1440} height={2880} alt="Lofi Flow Logo" />
         </div>
-        <div className="mb-5">
+        <div className="mb-2">
           {navMenu.map((link) => {
             const LinkIcon = link.icon;
             return (
@@ -41,6 +55,33 @@ const Sidebar = () => {
               </Link>
             )
           })}
+        </div>
+        <div className="mb-5">
+          {musicMenu.map((link) => {
+            const LinkIcon = link.icon;
+            return (
+              <Link
+                key={link.name}
+                href={link.href}
+                className="flex items-center">
+
+                <LinkIcon className="w-6 mr-4" />
+                <p>{link.name}</p>
+              </Link>
+            )
+          })}
+        </div>
+        <div className="divider mb-0"></div>
+        <div className="h-3/5 overflow-y-auto py-5 pt-1">
+          <ul>
+
+            {playlist.map((link) => (
+              <li>
+                {link}
+              </li>
+            )
+            )}
+          </ul>
         </div>
       </div>
     </div>
