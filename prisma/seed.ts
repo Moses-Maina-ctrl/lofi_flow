2f718490c6942104079dfce2fe16e234c8985033
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-05-23 22:57:00 +0300

    Add seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,2 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcrypt'
+import { artistsData } from "./songsData";
+
+const prisma = new PrismaClient()
+
+const main = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            }))
+          }
+        },
+        update: {}
+
+      })
+    }))
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
