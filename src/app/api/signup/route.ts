b3a301460188a39191cffe8e22c089b282db1e68
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-05-27 23:34:41 +0300

    Fix post function in sign route

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -5,7 +5,7 @@ import prisma from '../../../../lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   const salty = bcrypt.genSaltSync()
   const { email, password } = req.body
 
