16f605d795097910daa187de8c7ec482a9d3a9b5
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-06-05 15:27:05 +0300

    Fix state in authForm

--- lib/mutation.ts
+++ lib/mutation.ts
@@ -1,8 +1,13 @@
 import fetcher from "./fetcher";
 
 export const auth = (
-  mode: 'signin' | 'signup',
+  mode: 'Sign In' | 'Sign Up',
   body: { email: string, password: string }
 ) => {
-  return fetcher(`/${mode}`, body)
+  return fetcher(`/${urlLink(mode)}`, body)
+}
+
+const urlLink = (n: string) => {
+  let url = n.split('').filter(char => char !== ' ').join('');
+  return (url.toLowerCase())
 }
---
+++ src/components/authForm.tsx
@@ -0,0 +1,43 @@
+import { auth } from "../../lib/mutation";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const AuthForm: React.FC<{ mode: 'Sign In' | 'Sign Up' }> = ({ mode }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setisLoading] = useState(false)
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setisLoading(true)
+    const user = await auth(mode, { email, password })
+    setisLoading(false)
+    router.push('/')
+
+  }
+  return (
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl" >
+      <form className="card-body" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder=" Email" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder=" Password" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className="form-control mt-6">
+          <button className={`btn btn-primary ${isLoading ? 'loading' : ''}`} type="submit"  >{mode}</button>
+        </div>
+      </form>
+    </div>
+  )
+
+}
+
+export default AuthForm
